82498706a0ff59b96356b16fd0c82b5e7eb6b9bb
hritik <hritikror@gmail.com>
2024-05-06 22:28:31 +0530

    final commit

--- DB/Connection.js
+++ DB/Connection.js
@@ -4,7 +4,7 @@ mongoose.set("strictQuery", false);
 
 mongoose
   .connect(
-    "mongodb+srv://hritik-ror:HRItik.123%40%23@cluster0.0oiaf.mongodb.net/"
+    ""
   )
   .then(() => console.log("MongoDB Connected"))
   .catch((e) => console.log(e));
